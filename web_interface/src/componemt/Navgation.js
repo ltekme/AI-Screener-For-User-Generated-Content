5badb7386fc375016967b369a283cbf43e51f812
Karl <karl@ltek.me>
2024-07-27 13:29:50 +0000

    home route complete

--- web_interface/src/componemt/Navgation.js
+++ web_interface/src/componemt/Navgation.js
@@ -7,7 +7,7 @@ let Navgation = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="/">AI Content Filter</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
--- web_interface/src/index.js
+++ web_interface/src/index.js
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Navgation from "./componemt/Navgation";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navgation from "./componemt/Navgation";
+
+import Home from "./pages";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Navgation />
+    <BrowserRouter>
+      <Navgation />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        {/* <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/sign-up" element={<SignUp />} /> */}
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
